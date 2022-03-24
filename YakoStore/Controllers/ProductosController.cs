7c8f7340f08ffa207e27cbdcf7276e1b5cb72808
Valeria_Morales <valek2920@gmail.com>
2022-03-24 10:49:09 -0600

    Added Project

---
+++ YakoStore/Controllers/ProductosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YakoStore.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
